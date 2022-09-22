987b136173998aba21c18603a89bf01a102ee65e
Shane Dinh <shane.dinh5@gmail.com>
2022-09-22 17:39:24 -0500

    initial commit / schemas started

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,18 @@
+// import the gql tagged template function
+const { gql } = require("apollo-server-express");
+
+// create our typeDefs
+const typeDefs = gql`
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook: User
+  }
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
