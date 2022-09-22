3f2ecde948d924cb0efa16af2f6ceaddb0691ed5
Shane Dinh <shane.dinh5@gmail.com>
2022-09-22 18:04:14 -0500

    typeDefs finished

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -10,7 +10,37 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook: User
+    saveBook(
+      author: String
+      description: String
+      title: String
+      bookId: String
+      image: String
+      link: String
+    ): User
+    removeBook(bookId: String): User
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: String
+    authors: [Author]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
   }
 `;
 
