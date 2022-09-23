e65a1d4c004c83d64c07cf0605c94397b1ec536b
Shane Dinh <shane.dinh5@gmail.com>
2022-09-22 19:05:54 -0500

    client-side queries created

---
+++ client/src/utils/queries.js
@@ -0,0 +1,19 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_ME = gql`
+  {
+    me {
+      _id
+      username
+      email
+      books {
+        bookId
+        author
+        title
+        description
+        image
+        link
+      }
+    }
+  }
+`;
